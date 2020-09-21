fc7e2ca127ae1cc1627f9a3e529506ff82727181
aveWorld <texnarb228@gmail.com>
2020-09-21 14:31:31 +0300

    add tooltip

--- src/components/NewVault/index.js
+++ src/components/NewVault/index.js
@@ -25,13 +25,23 @@ import VaultTopImg from '../../assets/svg/vault_top-img.svg'
                 <VaultHeader>
                     <div className="header__title">
                         <h3 className="header__title"><span><ReactSVG src={TitleImg} className="header__img"/></span> NEW VAULT</h3> 
-                        <span className="header__question"><QuestionMark>?</QuestionMark></span>
+                        <span className="header__question">
+                            <QuestionMark>?</QuestionMark>
+                            <div className="header__tooltip">
+                                <p className="tooltip__p">Vault will be created in My Wallet. To create a vault which uses a Ledger device for signing transactions, create a vault in a Ledger wallet.</p>
+                            </div>
+                        </span>
                     </div>
                     <p className="vault__text">--</p>
                     <p className="vault__text fixed-width">A vault is an enhanced account with extra security and spending features.</p>
                 </VaultHeader>
                 <VaultMain>
-                    <div className="vault__text">Vault name <QuestionMark>?</QuestionMark> . . . . . . . . . . . . . . . . .</div>
+                    <div className="vault__text">Vault name <span className="main__question">
+                        <QuestionMark>?</QuestionMark>
+                        <div className="main__tooltip">
+                                <p className="tooltip__p">Vault will be created in My Wallet. To create a vault which uses a Ledger device for signing transactions, create a vault in a Ledger wallet.</p>
+                        </div>
+                        </span>  . . . . . . . . . . . . . . . . .</div>
                     <input type="text" className="vault__input" placeholder="My Vault name"/>
                 </VaultMain>
                 <VaultFooter>
@@ -108,9 +118,26 @@ import VaultTopImg from '../../assets/svg/vault_top-img.svg'
         display: flex;
         margin-top: 12px;
         margin-left: 5px;
+        &:hover .header__tooltip{
+            display: block;
+        }
     }
     .header__img {
         margin-right: 5px;
+    }
+    .header__tooltip {
+        display: none;
+        position: absolute;
+        width: 250px;
+        height: 72px;
+        background-color: white;
+        font-size: 12px;
+        border: 1px solid black;
+        z-index: 1;
+    }
+    .tooltip__p {
+        text-align: center;
+        vertical-align: center;
     }
     @media screen and (max-width: 992px) {
         .header__title {
@@ -128,6 +155,29 @@ import VaultTopImg from '../../assets/svg/vault_top-img.svg'
         width: 238px;
         height: 28px;
         margin-left: 30px;
+    }
+    .main__question {
+        position: relative;
+        &:hover .main__tooltip{
+            display: block;
+        }
+    }
+    .main__tooltip {
+        display: none;
+        position: absolute;
+        width: 250px;
+        height: 72px;
+        background-color: white;
+        font-size: 12px;
+        border: 1px solid black;
+        z-index: 1;
+        left: 0;
+        top: 0;
+        margin-top: 4px;
+    }
+    .tooltip__p {
+        text-align: center;
+        vertical-align: center;
     }
     @media screen and (max-width: 992px) {
         display: block;
@@ -165,6 +215,8 @@ import VaultTopImg from '../../assets/svg/vault_top-img.svg'
     color: white;
     text-align: center;
     display: inline-block;
+    position: relative;
+    z-index: 9;
   `
 
 export default NewVault;
